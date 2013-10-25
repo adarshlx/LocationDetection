79dd7c79d821d91e2abd1761774c08737f969c82
Pavan Kapanipathi <pavan046@gmail.com>
2013-10-25 16:32:54 -0500

    adding JaccardCoefficientSimilarityCalculator.java

---
+++ src/org/knoesis/locationdetection/similarity/JaccardCoefficientSimilarityCalculator.java
@@ -0,0 +1,54 @@
+package org.knoesis.locationdetection.similarity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class calculates the similarity using JackardCoefficient.
+ * 
+ * @author pavan
+ *        
+ * TODO: Pramod this is a good exercise for u to learn. Make all the similarity 
+ *                  measures as an interface. 
+ */
+public class JaccardCoefficientSimilarityCalculator {
+        
+        /**
+         * This method returns the jaccard coefficient of given two lists.
+         * @param docOne
+         * @param docTwo
+         * @return
+         */
+        public static double calculate(Set<String> docOne, Set<String> docTwo){
+                
+                double jaccardCoeff = 0;
+                
+                Set<String> intersectionOfSets = new HashSet<String>(docOne);
+                intersectionOfSets.retainAll(docTwo);
+                
+                Set<String> unionOfSets = new HashSet<String>(docOne);
+                unionOfSets.addAll(docTwo);
+                
+                jaccardCoeff = (double)intersectionOfSets.size() / unionOfSets.size();
+                
+                return jaccardCoeff;
+        }
+        
+                
+        public static void main(String[] args) {
+                Set<String> list1 = new HashSet<String>();
+                Set<String> list2 = new HashSet<String>();
+                
+                list1.add("Pramod");
+                list1.add("koneru");
+                list1.add("asdd");
+                
+                list2.add("asdd");
+                list2.add("Pramod");
+                list2.add("asas");
+                list2.add("aaas");
+                
+                System.out.println("The jaccard coeff is :" + JaccardCoefficientSimilarityCalculator.calculate(list1, list2));
+        }
+        
+}
